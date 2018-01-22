c6f8d717744e7207a3bf8c5858396d9e007a971b
Fahim Chowdhury <fahim.chowdhury1985@gmail.com>
2018-01-22 23:01:44 +0000

    working on home screen

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,5 +1,4 @@
 <template src="./template/template.html">
 </template><script src="./src/component.js">
-</script><style lang="scss" src="./style/core/core.scss"></style>
 </script><style lang="scss" scoped src="./style/style.scss"></style>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login/login'
+import Home from '@/components/home/home'
 
 Vue.use(Router)
-
-export default new Router({
+let authenticated = true; 
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
     }
   ]
+});
+
+
+router.beforeEach ((to, from,next) => {
+  console.log(to)
+    if (!authenticated && to.name !== 'Login') {
+        next(false);
+    } else {
+        next()
+    }
 })
+
+export default router;
