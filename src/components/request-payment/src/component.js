7f1ec05e50761c8bbd6bdd778d607b74a0de132c
fahim chowdhury <fahim@gmx.com>
2018-02-01 16:45:59 +0000

    added input for amount on qr generator

--- src/components/request-payment/src/component.js
+++ src/components/request-payment/src/component.js
@@ -2,15 +2,24 @@ import Store from '../../../model/model.js';
 
 export default {
   name: 'request-payment',
+  data() {
+    return {
+      amount: 0
+    }
+  },
+  methods: {
+    generate() {
+      new QRCode(document.getElementById("qrcode"), {
+        text: `${Store.address}__${this.amount}`,
+        width: 300,
+        height: 300,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.H
+      });
+    }
+  },
   mounted() {
-    console.log('here',Store);
-    new QRCode(document.getElementById("qrcode"), {
-      text: `${Store.address}_50`,
-      width: 300,
-      height: 300,
-      colorDark : "#000000",
-      colorLight : "#ffffff",
-      correctLevel : QRCode.CorrectLevel.H
-    });
+
   }
 };
